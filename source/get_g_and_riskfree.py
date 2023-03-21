1196e461897a738c98f48da57e07c95ff12d5ce0
rosieiiiii <72005846+rosieiiiii@users.noreply.github.com>
2023-03-21 00:40:08 -0700

    Update get_g_and_riskfree.py

--- source/get_g_and_riskfree.py
+++ source/get_g_and_riskfree.py
@@ -4,7 +4,7 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--dir_in', type=str, default='../yield_panel_daily_frequency_daily_maturity.csv')
+parser.add_argument('--dir_in', type=str, default='yield_panel_daily_frequency_daily_maturity.csv')
 args = parser.parse_args()
 
 y_KR = pd.read_csv(args.dir_in, index_col = 0)
@@ -14,5 +14,5 @@ Nmax = y_KR.shape[1]
 g_KR = np.exp(-y_KR*np.arange(1,Nmax+1)/365)
 df_kr = (1/g_KR['1']-1).to_frame(name='KR_LS')
 
-g_KR.to_pickle('../data_supplement/df_kr_g.pkl')
-df_kr.to_pickle('../data_supplement/df_riskfree_daily_all.pkl')
+g_KR.to_pickle('./data_supplement/df_kr_g.pkl')
+df_kr.to_pickle('./data_supplement/df_riskfree_daily_all.pkl')
